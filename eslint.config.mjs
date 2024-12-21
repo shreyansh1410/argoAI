e3ef9e4980f0dc7e3a580161529fdee8b41c1a32
Shreyansh Shukla <shreyansh.14010@gmail.com>
2024-12-21 16:11:40 +0530

    eslint updates

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,7 +1,14 @@
+import { dirname } from "path";
+import { fileURLToPath } from "url";
 import { FlatCompat } from "@eslint/eslintrc";
 
-const compat = new FlatCompat();
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = dirname(__filename);
 
-const eslintConfig = compat.extends(["next/core-web-vitals"]);
+const compat = new FlatCompat({
+  baseDirectory: __dirname,
+});
+
+const eslintConfig = [...compat.extends("next/core-web-vitals")];
 
 export default eslintConfig;
